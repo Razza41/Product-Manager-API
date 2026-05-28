b63b7ba21e113f6ec8d7744b66962f028efbe496
Ricardo Razza <razzaenascimento@outlook.com>
2026-05-28 20:02:24 -0300

    Commit inicial  - Product Manager API

---
+++ src/test/java/api/product/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package api.product.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
